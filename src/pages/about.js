1af3b77bb58d4018cd6751faa8a9fc1d8f2672d8
Luis Gomez <luey714@gmail.com>
2022-01-29 02:35:49 -0600

    added seo and social sharing tags

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,12 +1,19 @@
 import * as React from 'react';
-
 import { Link } from 'gatsby';
+
+import { Seo } from '../components/seo';
 
 export default function AboutPage() {
     return (
-        <main>
-            <h1>About this site!</h1>
-            <Link to="/">Back to Home</Link>
-        </main>
+        <>
+            <Seo
+                title='About this site'
+                description='More info about this site'
+            />
+            <main>
+                <h1>About this site!</h1>
+                <Link to="/">Back to Home</Link>
+            </main>
+        </>
     )
 }
