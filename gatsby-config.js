d44a4fb640489c90ba4c3c004af73fd655243998
Luis Gomez <luey714@gmail.com>
2022-01-29 00:09:46 -0600

    added site metadata

---
+++ gatsby-config.js
@@ -0,0 +1,9 @@
+module.exports = {
+    siteMetadata: {
+        siteUrl: 'https://www.mydomain.tld',
+        title: 'Blog application',
+        description: 'An application that allows for blogs to be created and posted',
+        image: 
+    'https://res.cloudinary.com/jlengstorf/image/upload/v1628127675/frontend-masters/gatsby-intro/share-image.jpg'
+    },
+}
--- src/pages/about.js
+++ src/pages/about.js
@@ -1,9 +1,12 @@
 import * as React from 'react';
 
+import { Link } from 'gatsby';
+
 export default function AboutPage() {
     return (
         <main>
             <h1>About this site!</h1>
+            <Link to="/">Back to Home</Link>
         </main>
     )
 }
