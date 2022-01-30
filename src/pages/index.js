577025591b56a0966bc9cdeea95792b9498e9f7e
Luis Gomez <luey714@gmail.com>
2022-01-30 00:10:15 -0600

    added query to render recent posts on homepage

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,14 +1,42 @@
 import * as React from 'react';
-import { Link } from 'gatsby';
+import { Link, useStaticQuery, graphql } from 'gatsby';
 import Layout from '../components/layout';
 
 
 export default function IndexPage() {
 
+    const data = useStaticQuery(graphql`
+        query GetBlogPosts {
+            allMdx(sort: {fields: frontmatter___date, order: DESC}) {
+                nodes {
+                    slug
+                    id
+                    frontmatter {
+                        date(fromNow: true)
+                        description
+                        title
+                    }
+                }
+            }
+        }
+    `);
+
+    const posts = data.allMdx.nodes;
+
     return (
         <Layout>
             <h1>Hello Friends!</h1>
             <Link to='/about'>About this site</Link>
+
+            <h2>Chcek out my recent blog posts</h2> 
+            <ul>
+                {posts.map((post) => (
+                    <li key={post.id}>
+                        <Link to={post.slug}>{post.frontmatter.title}</Link>{' '}
+                        <small>Posted {post.frontmatter.date}</small>
+                    </li>
+                ))}
+            </ul>
         </Layout>
     );
 }
