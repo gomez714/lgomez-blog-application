f551f6385da9a40eb7802e45175d74ed29594fce
Luis Gomez <luey714@gmail.com>
2022-01-31 16:20:55 -0600

    added images to site, used gatsby plugins for optimization

--- gatsby-config.js
+++ gatsby-config.js
@@ -25,11 +25,28 @@ module.exports = {
         {
             resolve: 'gatsby-plugin-mdx',
             options: {
+                gatsbyRemarkPlugins: [
+                    {
+                        resolve: 'gatsby-remark-images',
+                        options: {
+                            maxWidth: 1200
+                        },
+                    },
+                ],
                 defaultLayouts: {
                     posts: require.resolve('./src/components/post-layout.js')
                 }
             },
         },
-
+        {
+            resolve: 'gatsby-source-filesystem',
+            options: {
+                name: 'images',
+                path: `${__dirname}/src/images`
+            },
+        },
+        'gatsby-plugin-image',
+        'gatsby-plugin-sharp',
+        'gatsby-transformer-sharp'
     ]
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,9 @@
 import * as React from 'react';
 import { Link, useStaticQuery, graphql } from 'gatsby';
-import Layout from '../components/layout';
+import { StaticImage } from 'gatsby-plugin-image';
 
+import Layout from '../components/layout';
+import { imageWrapper } from '../styles/index.module.css';
 
 export default function IndexPage() {
 
@@ -25,6 +27,15 @@ export default function IndexPage() {
 
     return (
         <Layout>
+            <div className={imageWrapper}>
+                <StaticImage
+                    src='../images/ivana-la-61jg6zviI7I-unsplash.jpg'
+                    alt='a corgi sitting on a bed with red paper hearts all over it. It looks unamused'
+                    placeholder='dominantColor'
+                    width={300}
+                    height={300}
+                />
+            </div>
             <h1>Hello Friends!</h1>
             <Link to='/about'>About this site</Link>
 
