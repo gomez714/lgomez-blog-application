7ffb9174d7725a7faad7c3a16d8bd83f9fe1fa7f
Luis Gomez <luey714@gmail.com>
2022-01-31 16:38:35 -0600

    added page queries to get images from gatsby

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,13 +1,29 @@
 import * as React from 'react';
-import { Link } from 'gatsby';
-
+import { Link, graphql } from 'gatsby';
+import { GatsbyImage, getImage } from 'gatsby-plugin-image';
 import Layout from '../components/layout'
-export default function AboutPage() {
+
+
+export const query = graphql`
+    query CocktailQuery {
+        file(name: {eq: "cocktail"}) {
+            childImageSharp {
+            gatsbyImageData(placeholder: DOMINANT_COLOR)
+            }
+        }
+    }
+`
+
+export default function AboutPage({ data }) {
     return (
         <Layout
             title='About this site'
             description='More info about this site'
         >   
+            <GatsbyImage
+                image={getImage(data.file)}
+                alt='a cocktail set insife an elaborate floral arrangement with dry ice mist curling around it'
+            />
             <h1>About this site!</h1>
             <Link to="/">Back to Home</Link>
         </Layout>
